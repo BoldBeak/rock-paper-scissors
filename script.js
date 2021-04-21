6dd84913c6ae80adb6cfbeba50139d86aff09517
boldbeak <boldbeakdev@gmail.com>
2021-04-21 05:10:57 -0500

    playRound function set up and returns expected results

--- script.js
+++ script.js
@@ -11,19 +11,32 @@ function playerPlay() {
   //return lowerInput;
 
   // ***** Random for setup/testing *****
-  let options = ["rock", "paper", "scissors"];
-  return options[Math.floor(Math.random() * options.length)];
+  let choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * choices.length)];
 }
-
-let scores = {"player": 0, "computer": 0, "tie": 0};
-let playerScore = scores.player;
-let computerScore = scores.computer;
-let tieScore = scores.tie;
 
 function playRound(playerSelection, computerSelection) {
+  console.log("p = " + playerSelection, "c = " + computerSelection);
   let resultOfRound = {"win": `You win! Your ${playerSelection} beat the computer's ${computerSelection}.`, "lose": `You lose! Computer's ${computerSelection} beat your ${playerSelection}.`, "tie": `It's a tie! You both selected ${playerSelection}. Try again!`};
+
+  if (playerSelection === computerSelection) {
+    return resultOfRound.tie;
+  } else
+  if (playerSelection === "rock" && computerSelection === "scissors") {
+    return resultOfRound.win;
+  } else
+  if (playerSelection === "paper" && computerSelection === "rock") {
+    return resultOfRound.win;
+  } else
+  if (playerSelection === "scissors" && computerSelection === "paper") {
+        return resultOfRound.win;
+  } else {
+    return resultOfRound.lose;
+  }
 }
 
+const playerSelection = playerPlay();
+const computerSelection = computerPlay();
 
 
-console.log(computerPlay(), playerPlay());
+console.log(playRound(playerSelection, computerSelection));
