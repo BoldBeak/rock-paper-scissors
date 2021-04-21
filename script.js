fa434789ef3d499e54a6661d6f983053a87f6b45
boldbeak <boldbeakdev@gmail.com>
2021-04-21 04:49:49 -0500

    started function to play single round. Set up result of round as an object.

--- script.js
+++ script.js
@@ -15,4 +15,15 @@ function playerPlay() {
   return options[Math.floor(Math.random() * options.length)];
 }
 
+let scores = {"player": 0, "computer": 0, "tie": 0};
+let playerScore = scores.player;
+let computerScore = scores.computer;
+let tieScore = scores.tie;
+
+function playRound(playerSelection, computerSelection) {
+  let resultOfRound = {"win": `You win! Your ${playerSelection} beat the computer's ${computerSelection}.`, "lose": `You lose! Computer's ${computerSelection} beat your ${playerSelection}.`, "tie": `It's a tie! You both selected ${playerSelection}. Try again!`};
+}
+
+
+
 console.log(computerPlay(), playerPlay());
